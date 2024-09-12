8da465f8437cd0b969cb6954195129c54407e559
sandytek <146525821+sandytek@users.noreply.github.com>
2024-09-12 20:10:18 +0800

    Add functions for all of the basic math operators git commit-m

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Project-Calculator
+The calculator will be able to perform basic functions such as add, subtract, multiply, divide.
---
+++ calculator.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNum;
+let secondNum;
+let operatorDisplay;
+
+function operate(operator, first, second) {
+    switch(operator) {
+        case "+": 
+            return add(first, second);
+        case "-":
+            return subtract(first, second);
+        case "*":
+            return multiply(first, second);
+        case "/":
+            return divide(first, second);
+    }
+}
