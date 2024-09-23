02598996287df4949409bb02bab95f1a68c51b8d
sandytek <146525821+sandytek@users.noreply.github.com>
2024-09-23 21:39:17 +0800

    added comments to guide decimal place implementation

--- calculator.js
+++ calculator.js
@@ -30,7 +30,12 @@ function operate(operation, first, second) {
             return multiply(first, second);
             
         case "/":
+            if (second == 0) {
+                alert("Hey! Don't do this!");
+                return '';
+            } else {
             return divide(first, second);
+            }
         }
     }
 
@@ -40,6 +45,9 @@ const opsButtons = document.querySelectorAll(".opsButton");
 const equal = document.querySelector(".equalButton");
 const clear = document.querySelector(".clearButton");
 
+// get the decimal button specially to control it
+// const decimalButton = Array.from(numButtons).find(button => button.textContent === '.');
+
 
 numButtons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -48,6 +56,16 @@ numButtons.forEach((button) => {
     });
 });
 
+// add a second event listener specially for the decimal button.
+// if decimal button is enabled, it is clickable,
+// i.e. it can add '.' to the number
+// so it should disable itself when clicked
+/*
+decimalButton.addEventListener("click", () => {
+    disableDecimalBtn();
+});
+*/
+
 opsButtons.forEach((button) => {
     button.addEventListener("click", () => {
         operator = button.textContent;
@@ -55,10 +73,15 @@ opsButtons.forEach((button) => {
     })
 })
 
-function opsButtonEventCallback(ops) {
+function opsButtonEventCallback() {
+    // when pressing ops button,
+    // calculator will be processing input for the next number.
+    // next number starts empty i.e. no decimal
+    // so enable decimal button
     if (firstNum == '' && secondNum == '') {
         firstNum = numDisplay;
         numDisplay = '';
+        // enableDecimalBtn();
     } else if (firstNum !== '' && secondNum == '' && numDisplay !== '') {
         secondNum = numDisplay;
         result = operate(operator, parseInt(firstNum), parseInt(secondNum));
@@ -66,6 +89,7 @@ function opsButtonEventCallback(ops) {
         firstNum = result;
         secondNum = '';
         numDisplay = '';
+        // enableDecimalBtn();
     } 
 }
 
@@ -91,4 +115,99 @@ clear.addEventListener("click", () => {
     secondNum = '';
     result = '';
     operator = '';
+    // since the entire number is cleared, decimal (if added) also disappears
+    // so enable decimal button
+    // enableDecimalBtn();
 });
+
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case "1":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "2":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "3":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "4":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "5":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "6":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "7":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "8":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "9":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "0":
+            numDisplay += event.key;
+            mainDisplay.textContent = `${numDisplay}`;
+            break;
+        case "=":
+            equalButtonEventCallback();
+            break;
+        case "+":
+            operator = event.key;
+            opsButtonEventCallback();
+            break;
+        case "-":
+            operator = event.key;
+            opsButtonEventCallback();
+            break;
+        case "/":
+            operator = event.key;
+            opsButtonEventCallback();
+            break;
+        case "*":
+            operator = event.key;
+            opsButtonEventCallback();
+            break;
+        case "Backspace":
+            if(numDisplay !== '') {
+                // slice numDisplay, but didnt assign the slice back to numDisplay
+                // i.e. did not change numDisplay
+                let sliced = numDisplay.slice(0,-1);
+                let prev = numDisplay.slice(-1);
+                numDisplay = sliced;
+                mainDisplay.textContent = `${numDisplay}`;
+                // if backspace removed a decimal point, enable the decimal button
+                /*
+                if(prev == ".") {
+                    enableDecimalBtn();
+                }
+                */
+            }
+            break;
+    }
+        
+        
+});
+
+/*
+function enableDecimalBtn() {
+    decimalButton.disabled = false;
+}
+
+function disableDecimalBtn() {
+    decimalButton.disabled = true;
+}
+*/
